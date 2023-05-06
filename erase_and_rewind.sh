c22e18732e4636b2afc3809530d04c53f975b580
Octavian David <2542748+octaviandavid@users.noreply.github.com>
2023-05-06 12:08:02 +0000

    added partitioning

--- erase_and_rewind.sh
+++ erase_and_rewind.sh
@@ -6,6 +6,7 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
+# .env check
 if [ -f ".env" ]; then
     export $(echo $(cat ".env" | sed 's/#.*//g'| xargs) | envsubst)
 else
@@ -14,8 +15,13 @@ else
     exit
 fi
 
+# config
 export DEV="/dev/${DISK}"
+export DM="${DEV##*/}"
+export DEVP="${DEV}$( if [[ "$DEV" =~ "nvme" ]]; then echo "p"; fi )"
+export DM="${DM}$( if [[ "$DM" =~ "nvme" ]]; then echo "p"; fi )"
 
+# present state dump
 lsblk
 
 sgdisk --print $DEV
@@ -23,5 +29,28 @@ sgdisk --print $DEV
 echo "WARNING! Formatting drive ${DEV}... Press ENTER to continue."
 read
 
+# execution
 sgdisk --zap-all $DEV
+
+
+sgdisk --new=1:0:+768M $DEV
+sgdisk --typecode=1:8301 $DEV 
+sgdisk --change-name=1:/boot $DEV
+
+sgdisk --new=2:0:+2M $DEV
+sgdisk --typecode=2:ef02 $DEV 
+sgdisk --change-name=2:GRUB $DEV
+
+sgdisk --new=3:0:+128M $DEV
+sgdisk --typecode=3:ef00 $DEV 
+sgdisk --change-name=3:EFI-SP $DEV 
+
+sgdisk --new=5:0:0 $DEV
+sgdisk --typecode=5:8301 $DEV
+sgdisk --change-name=5:rootfs $DEV
+
+sgdisk --hybrid 1:2:3 $DEV
+
+sgdisk --print $DEV
+
 
