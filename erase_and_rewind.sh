b766d8d2eb51cf123d90de9773e6b2d3ad24671e
Octavian David <2542748+octaviandavid@users.noreply.github.com>
2023-05-06 14:21:20 +0300

    run as root check

--- erase_and_rewind.sh
+++ erase_and_rewind.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+# run as root check
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 if [ -f ".env" ]; then
     export $(echo $(cat ".env" | sed 's/#.*//g'| xargs) | envsubst)
 fi
